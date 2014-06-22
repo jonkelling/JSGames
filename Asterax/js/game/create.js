4c351733fd4a4ac6fe5cf16051339e69ea585ea1
jonkelling <jonkelling@Jons-MacBook-Air.local>
2014-06-22 10:03:04 -0500

    more iPad 1 testing

--- Asterax/js/game/create.js
+++ Asterax/js/game/create.js
@@ -14,7 +14,7 @@ define(['player', 'rock', 'rockGroupController'], function (Player, Rock, RockGr
 			game.renderer.clearBeforeRender = false;
 			game.renderer.roundPixels = true;
 			
-			//app.background = game.add.tileSprite(0, 0, 800, 600, 'background');
+			app.background = game.add.tileSprite(0, 0, 800, 600, 'background');
 			game.antialias = false;
 			
 			//	Enable p2 physics
