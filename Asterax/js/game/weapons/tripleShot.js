3d429487f43d6f564e9f7505ad305945bfef434d
jonkelling <jonkelling@Jons-MacBook-Air.local>
2014-07-05 23:36:13 -0500

    more weapon stuff

---
+++ Asterax/js/game/weapons/tripleShot.js
@@ -0,0 +1,44 @@
+
+define(['twinShot', 'weapon'], function(TwinShot, Weapon) {
+
+	var module = function()
+	{
+		TwinShot.call(this, "tripleShot");
+		
+		this.firingSecondShots = false;
+		this.firingThirdShot = false;
+	}
+
+	module.prototype = Object.create(TwinShot.prototype);
+	module.prototype.constructor = module;
+	
+	module.prototype.fire = function(force)
+	{
+		if (this.group.countDead() < 3 && !this.firingSecondShot && !this.firingThirdShot)
+		{
+			return;
+		}
+		Weapon.prototype.fire.call(this, force);
+	};
+	
+	module.prototype.afterFire = function(bullet)
+	{
+		if (this.firingThirdShot == false)
+		{
+			this.firingThirdShot = true;
+			TwinShot.prototype.afterFire.call(this, bullet);
+			this.fire(true);
+		}
+		else if (this.firingSecondShot)
+		{
+			TwinShot.prototype.afterFire.call(this, bullet);
+		}
+		else
+		{
+			this.firingThirdShot = false;
+		}
+	};
+	
+	return module;
+
+});
